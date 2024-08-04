cf01b760273a1af833ce5cc0d4028ec69873aea7
Nouma123 <noumajameel@hotmail.com>
2024-08-04 23:32:28 +0100

    updating code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
-import reactImg from '../assets/react-core-concepts.png';
+import reactImg from '../../assets/react-core-concepts.png';
+import './Header.css';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max * 1));
+  return Math.floor(Math.random() * (max + 1));
 }
 
 export default function Header() {
-  const description = reactDescriptions[genRandomInt(3)];
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
